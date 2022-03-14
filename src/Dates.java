1b1dbeac9fb896420cc37c5cbc906a31e9e91e31
Long <loilonggia@gmail.com>
2022-03-15 03:36:01 +0700

    adding Student and Dates class

---
+++ src/Dates.java
@@ -0,0 +1,108 @@
+public class Dates {
+    private int day;
+    private int month;
+    private int year;
+
+    //can be change later to suit the corresponding era
+    static int MAX_YEAR = 9999;
+    static int MIN_YEAR = 1800;
+
+    static boolean isLeapYear(int year) {
+        //the year is leap (366 days instead of 365)
+        //is when it is divisible to 4 AND NOT to 100
+        //OR it can be divisible to 400
+        return (((year % 4 == 0) && year % 100 != 0)
+                || year % 400 == 0);
+    }
+    //return true if month contain 31 days else false
+    static boolean is31Days(int month) {
+        return (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12);
+    }
+    static boolean isValidDate(int day, int month, int year) {
+        if (year < MIN_YEAR || year > MAX_YEAR) {
+            System.out.println("A year must be in range of 1800 to 9999!");
+            return false;
+        }
+        if (month < 1 || month > 12) {
+            System.out.println("A month must be in range of 1 to 12!");
+            return false;
+        }
+        if (day < 1 || day > 31) {
+            System.out.println("A date must at least be in range of 1 to 31!");
+            return false;
+        }
+
+        //check for the 2nd month for leap year
+        if (month == 2) {
+            if (isLeapYear(year)) {
+                if (day > 29){
+                    System.out.println("A leap-year 2nd month must at least be in range of 1 to 29!");
+                }
+                return (day <= 29);
+            }
+            else {
+                if (day > 28){
+                    System.out.println("A non-leap-year 2nd month must at least be in range of 1 to 28!");
+                }
+                return (day <= 28);
+            }
+        }
+
+        //since months with 31 and the 2nd month has been dealt with
+        //moving to the 30s
+        if (month == 4 || month == 6 || month == 9 || month == 11) {
+            if (day > 30){
+                System.out.println("The month '\"'" + Integer.toString(month) + "'\"' must at least be in range of 1 to 30!");
+            }
+            return (day <= 30);
+        }
+        else {
+            //System.out.println("Oh no...");
+            return (day < 31);
+        }
+
+    }
+
+
+    public Dates(){
+        day = 01;
+        month = 01;
+        year = MIN_YEAR;
+    }
+    public Dates(int month, int day, int year) {
+        if (isValidDate(day, month, year)) {
+            this.day = day;
+            this.month = month;
+            this.year = year;
+        }
+        else {
+            day = 01;
+            month = 01;
+            year = MIN_YEAR;
+        }
+    }
+
+    public  boolean setDate(Dates newDate){
+        if (isValidDate(newDate.day, newDate.month, newDate.year)) {
+            this.day = newDate.day;
+            this.month = newDate.month;
+            this.year = newDate.year;
+            return true;
+        }
+        return false;
+    }
+
+
+    //to print out the date in mm/dd/yy format
+    @Override
+    public String toString() {
+        return (Integer.toString(month) + '/' + Integer.toString(day) + '/' + Integer.toString(year));
+    }
+    /*
+    //to print out the date in dd/mm/yy format
+    @Override
+    public String toString() {
+        return (Integer.toString(day) + '/' + Integer.toString(month) + '/' + Integer.toString(year));
+    }
+    */
+}
---
+++ src/Student.java
@@ -0,0 +1,36 @@
+public class Student {
+    private String studentID;
+    private String studentName;
+    private Dates dayOfBirth;
+
+    public Student() {
+        studentID = "s000";
+        studentName = "Default";
+        dayOfBirth = null;
+    }
+    public Student(String studentName, String studentID, Dates dayOfBirth) {
+        this.studentID = studentID;
+        this.studentName = studentName;
+        this.dayOfBirth = dayOfBirth;
+    }
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+    public void setDate(Dates dayOfBirth) {
+        this.dayOfBirth = dayOfBirth;
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+    public String getStudentName() {
+        return studentName;
+    }
+    public Dates getDayOfBirth() {
+        return dayOfBirth;
+    }
+}
